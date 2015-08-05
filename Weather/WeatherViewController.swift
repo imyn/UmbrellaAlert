9b05d1631aa7b3b06adde1111a379ebc885cfb12
Yerin Im <yerin.im43@gmail.com>
2015-08-04 17:04:48 -0700

    Put data in an array

--- Weather/WeatherViewController.swift
+++ Weather/WeatherViewController.swift
@@ -51,18 +51,23 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         }
         task.resume()
     }
+    
+    
     func displayWeather(weatherData: NSData)
-        
-    {    var error: NSError?
+    {
+        var rainArray: [Double]!
+        var error: NSError?
         let jsonObject : AnyObject! = NSJSONSerialization.JSONObjectWithData(weatherData, options: nil, error: &error) as! NSDictionary
         
         
         //Return precipitation volume mm per 3 hours
         if let rain0 = jsonObject["list"]!!.objectAtIndex(0).objectForKey("rain")?.objectForKey("3h") as? Double {
             println(rain0)
+            rainArray.append(rain0)
+            println(rainArray)
         }
         
-        else {println("No rain found 0")
+        else {println("0.0")
             
         }
         
@@ -72,10 +77,13 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         }
         
         if let rain = jsonObject["list"]!!.objectAtIndex(1).objectForKey("rain")?.objectForKey("3h") as? Double {
-            println("\(rain)")
+            println(rain)
+            rainArray.append(rain)
+            println(rainArray)
         }
-        else {
-            println("No rain found")
+        else
+        {
+            println("0.0")
         }
         
         if let dtTime = jsonObject["list"]!!.objectAtIndex(1).objectForKey("dt_txt") as? String{
@@ -83,62 +91,100 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             
         }
         
-        if let rain2 = jsonObject["list"]!!.objectAtIndex(2).objectForKey("rain")?.objectForKey("3h") as? String {
-            println("\(rain2)")
+        if let rain2 = jsonObject["list"]!!.objectAtIndex(2).objectForKey("rain")?.objectForKey("3h") as? Double {
+            println(rain2)
+            rainArray.append(rain2)
+            println(rainArray)
             
         }
         else {
-            println("No rain found2")
+            println("0.0")
         }
         
         if let dtTime2 = jsonObject["list"]!!.objectAtIndex(2).objectForKey("dt_txt") as? String {
             println(dtTime2)
         }
         
-        if let rain3 = jsonObject["list"]!!.objectAtIndex(3).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain3)}
-        else{println("No rain found3")}
+        if let rain3 = jsonObject["list"]!!.objectAtIndex(3).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain3)
+            rainArray.append(rain3)
+            println(rainArray)
+        
+        }
+        
+        else {println("0.0")}
         
         if let dtTime3 = jsonObject["list"]!!.objectAtIndex(3).objectForKey("dt_txt") as? String {
             println(dtTime3)
+
         }
         
-        if let rain4 = jsonObject["list"]!!.objectAtIndex(4).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain4)}
-        else{println("No rain found4")}
+        if let rain4 = jsonObject["list"]!!.objectAtIndex(4).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain4)
+            rainArray.append(rain4)
+            println(rainArray)
+        
+        }
+        
+        else
+        {println("0.0")}
         
         if let dtTime4 = jsonObject["list"]!!.objectAtIndex(4).objectForKey("dt_txt") as? String {
             println(dtTime4)
         }
         
-        if let rain5 = jsonObject["list"]!!.objectAtIndex(5).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain5)}
-        else{println("No rain found5")}
+        if let rain5 = jsonObject["list"]!!.objectAtIndex(5).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain5)
+            rainArray.append(rain5)
+            println(rainArray)
+        
+        }
+        else{println("0.0")}
         
         if let dtTime5 = jsonObject["list"]!!.objectAtIndex(5).objectForKey("dt_txt") as? String {
             println(dtTime5)
         }
         
-        if let rain6 = jsonObject["list"]!!.objectAtIndex(6).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain6)}
-        else{println("No rain found6")}
+        if let rain6 = jsonObject["list"]!!.objectAtIndex(6).objectForKey("rain")?.objectForKey("3h") as? Double { println("\(rain6)")
+            rainArray.append(rain6)
+            println(rainArray)
+        
+        
+        }
+        
+        else{println("0.0")}
         
         if let dtTime6 = jsonObject["list"]!!.objectAtIndex(6).objectForKey("dt_txt") as? String {
             println(dtTime6)
         }
         
-        if let rain7 = jsonObject["list"]!!.objectAtIndex(7).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain7)}
-        else{println("No rain found7")}
+        if let rain7 = jsonObject["list"]!!.objectAtIndex(7).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain7)
+            rainArray.append(rain7)
+            println(rainArray)
+        
+        }
+        
+        else{println("0.0")}
         
         if let dtTime7 = jsonObject["list"]!!.objectAtIndex(7).objectForKey("dt_txt") as? String {
             println(dtTime7)
         }
         
-        if let rain8 = jsonObject["list"]!!.objectAtIndex(8).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain8)}
-        else{println("No rain found8")}
+        if let rain8 = jsonObject["list"]!!.objectAtIndex(8).objectForKey("rain")?.objectForKey("3h") as? Double { println(rain8)
+            rainArray.append(rain8)
+            println(rainArray)
+        
+        }
+        
+        
+        else{println("0.0")}
         
         if let dtTime8 = jsonObject["list"]!!.objectAtIndex(8).objectForKey("dt_txt") as? String {
             println(dtTime8)
         }
         
-        
-        
-        
     }
+    
+    
+    
+    
 }
+    
+
